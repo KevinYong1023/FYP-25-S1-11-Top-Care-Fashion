e7bb1b2ffcf938eb55694c7716d14b561b41b76b
trii00 <148936864+trii00@users.noreply.github.com>
2025-03-20 16:00:36 +0800

    Update userHeader.js

--- src/Components/Headers/userHeader.js
+++ src/Components/Headers/userHeader.js
@@ -35,7 +35,7 @@ export default function UserHeader({ loginStatus }) {
                             <Nav>
                                 {isLoggedIn ? (
                                     <>  
-                                        <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                                        <Nav.Link as={Link} to="/user-profile">Profile</Nav.Link>
                                         <Nav.Link as={Link} to="/logout">Logout</Nav.Link>
                                         <Nav.Link as={Link} to="/cart">
                                         <FaShoppingCart />
