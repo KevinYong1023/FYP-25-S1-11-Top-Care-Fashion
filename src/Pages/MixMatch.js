bd7870c71bf625acdbd88474ba3351889a2a3c14
KevinCode-web <kevinyong1023@gmail.com>
2025-04-06 20:01:06 +0800

    mix and match page

--- src/Pages/MixMatch.js
+++ src/Pages/MixMatch.js
@@ -6,14 +6,15 @@ import { Container, Row, Col, Form, Button, Card } from "react-bootstrap";
 const MixMatch = () => {
     const [occasion, setOccasion] = useState("");
 
-    const handleMixMatch = () => {
-        console.log("Mixing for:", occasion);
+    function handleMixMatch(e){
+        e.preventDefault(); 
         // Add your logic here
         alert("TRIGGER MIX AND MATCH")
-    };
+    }
 
-    const addProductsToCart = () =>{
-        alert("TRIGGER ADD ALL 3 ITEMS TO CART")
+    function addProductsToCart(){
+        // Add your logic here
+        alert("TRIGGER Add to cart")
     }
 
     return (
@@ -24,13 +25,15 @@ const MixMatch = () => {
                 {/* Input Section */}
                 <Row className="justify-content-center mb-4">
                     <Col md={6} className="d-flex align-items-center gap-2">
+                    <Form onSubmit={ handleMixMatch}>
                         <Form.Control
                             type="text"
                             placeholder="Input an occasion"
                             value={occasion}
                             onChange={(e) => setOccasion(e.target.value)}
                         />
-                        <Button onClick={handleMixMatch}>Mix-And-Match</Button>
+                        <Button type="submit">Mix-And-Match</Button>
+                    </Form>
                     </Col>
                 </Row>
 
@@ -93,7 +96,7 @@ const MixMatch = () => {
                             <Card.Text>Seller: DEF</Card.Text>
                         </Card>
 
-                        <Button variant="success" className="mt-2" onClick={addProductsToCart()}>Add to Cart</Button>
+                        <Button variant="success" className="mt-2" onClick={addProductsToCart}>Add to Cart</Button>
                     </Col>
                 </Row>
             </Container>
